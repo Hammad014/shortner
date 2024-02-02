6c19ec0aa18f6ef8e8a25a2d685623aa126dccfd
Hammad014 <hammadworld011@gmail.com>
2024-02-02 20:07:07 +0500

    added login

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,11 +1,12 @@
 import Image from "next/image";
 import Footer from "./components/Footer";
-import Main from './main/page'
+import Main from './main/page';
+import Navbar from "./components/Navbar";
 
 export default function Home() {
   return (
     <>
-
+   <Navbar showSignIn={true} showRegister={true}/>
      <Main/>
     <Footer/>
     </>
