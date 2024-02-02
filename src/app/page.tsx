757800c6d3f9fd6f891ca19c0c7ec781ff316186
Hammad014 <hammadworld011@gmail.com>
2024-02-02 19:32:27 +0500

    just front page

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,12 +1,11 @@
 import Image from "next/image";
 import Footer from "./components/Footer";
 import Main from './main/page'
-import Navbar from "./components/Navbar";
 
 export default function Home() {
   return (
     <>
-   <Navbar showSignIn={true} showRegister={true}/>
+
      <Main/>
     <Footer/>
     </>
